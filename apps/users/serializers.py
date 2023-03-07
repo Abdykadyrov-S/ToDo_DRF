f77db9a0b67801f85e14f886a307caec7270f43a
Simon070805 <abdykadyrovsyimyk0708>
2023-03-07 15:58:01 +0600

    users - api

---
+++ apps/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from .models import User
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "usernsme", "email", "phone_number", "age")
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length = 255, write_only=True
+    )
+    password2 = serializers.CharField(
+        max_length = 255, write_only=True
+    )
+
+    class Meta:
+        model = User 
+        fields = ("id", "username", "email", "phone_number", "age", "password", "password2")
+
+    def validate(self,attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password":'Пароли не совпадают'})
+        if '+996' not in attrs['phone_number']:
+            raise serializers.ValidationError({'phone_number': "Начните номер с +996"})
+        return attrs
+    
+    def create(self, values):
+        user = User.objects.create(
+            username = values['username'],
+            email = values['email'],
+            phone_number = values['phone_number'],
+            age = values['age']
+        )
+        user.set_password(values['password'])
+        user.save()
+        return user
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+from .serializers import User, UserRegisterSerializer, UserSerializers
 
 # Create your views here.
+class UserAPIViewSet(GenericViewSet,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializers
+
+    def get_serializer_class(self):
+        if self.action in ('create', ):
+            return UserRegisterSerializer
+        return UserRegisterSerializer
+        
+    
